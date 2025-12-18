3f638b2ea824ded4ccfc15cac0f5d291c879a606
ZombieCheese <149222504+zombiecheese@users.noreply.github.com>
2025-12-19 00:06:19 +1100

    Update update.sh

--- update.sh
+++ update.sh
@@ -5,29 +5,25 @@ if [[ $EUID -eq 0 ]]; then
   exit 1
 fi
 
-if [ -d "now-playing" ]; then
-
-  echo "==> Stopping now-playing systemd service..."
-  if [ -f "/etc/systemd/system/now-playing.service" ]; then
-      sudo systemctl stop now-playing
-  fi
-  echo "==> fetching updated code from git"
-  cd now-playing
-
-  git init
-  git remote remove origin
-  git remote add origin https://github.com/zombiecheese/now-playing
-  git fetch origin
-  git reset --hard origin/main
-  
-  install_path=$(pwd)
-  source "${install_path}/venv/bin/activate" && echo "✔ Virtual environment activated."
-  echo "==> upgrading required Python packages..."
-  pip3 install -r requirements.txt --upgrade && echo "✔ Python packages installed successfully."
-  echo "Dectivating the virtual environment..."
-  source "${install_path}/venv/bin/deactivate" && echo "✔ Virtual environment activated."
+echo "==> Stopping now-playing systemd service..."
+if [ -f "/etc/systemd/system/now-playing.service" ]; then
+  sudo systemctl stop now-playing
 fi
+echo "==> fetching updated code from git"
+
+git init
+git remote remove origin
+git remote add origin https://github.com/zombiecheese/now-playing
+git fetch origin
+git reset --hard origin/main
+
+install_path=$(pwd)
+source "${install_path}/venv/bin/activate" && echo "✔ Virtual environment activated."
+echo "==> upgrading required Python packages..."
+pip3 install -r requirements.txt --upgrade && echo "✔ Python packages installed successfully."
+echo "Dectivating the virtual environment..."
+source "${install_path}/venv/bin/deactivate" && echo "✔ Virtual environment activated."
+
 
 echo "==> run systemctl start now-playing"
 echo "🎉 Update is complete! Your now-playing display is configured."
-exit
